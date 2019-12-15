e7d4c8b6913c319c773a57373d14bcc5daffe429
Alex-DG <alexandre.diguida@gmail.com>
2019-12-15 17:35:38 +0000

    🎨 Improve code - play with mesh & VAO

--- Shaders.js
+++ Shaders.js
@@ -1,4 +1,72 @@
+class Shader {
+  constructor(gl, vertShaderSrc, fragShaderSrc) {
+    this.program = ShaderUtil.createProgramFromText(
+      gl,
+      vertShaderSrc,
+      fragShaderSrc,
+      true
+    );
+
+    if (this.program != null) {
+      this.gl = gl;
+      gl.useProgram(this.program);
+      this.attribLoc = ShaderUtil.getStandardAttribLocations(gl, this.program);
+      this.uniformLoc = {}; //TODO : Replace in later lessons with get standardUniformLocations.
+    }
+
+    //Note :: Extended shaders should deactivate shader when done calling super and setting up custom parts in the constructor.
+  }
+
+  //...................................................
+  //Methods
+  activate() {
+    this.gl.useProgram(this.program);
+    return this;
+  }
+  deactivate() {
+    this.gl.useProgram(null);
+    return this;
+  }
+
+  //function helps clean up resources when shader is no longer needed.
+  dispose() {
+    //unbind the program if its currently active
+    if (this.gl.getParameter(this.gl.CURRENT_PROGRAM) === this.program)
+      this.gl.useProgram(null);
+    this.gl.deleteProgram(this.program);
+  }
+
+  //...................................................
+  //RENDER RELATED METHODS
+
+  //Setup custom properties
+  preRender() {} //abstract method, extended object may need need to do some things before rendering.
+
+  //Handle rendering a modal
+  renderModal(modal) {
+    this.gl.bindVertexArray(modal.mesh.vao); //Enable VAO, this will set all the predefined attributes for the shader
+
+    if (modal.mesh.indexCount)
+      this.gl.drawElements(
+        modal.mesh.drawMode,
+        modal.mesh.indexCount,
+        gl.UNSIGNED_SHORT,
+        0
+      );
+    else this.gl.drawArrays(modal.mesh.drawMode, 0, modal.mesh.vertexCount);
+
+    this.gl.bindVertexArray(null);
+
+    return this;
+  }
+}
+
 class ShaderUtil {
+  //-------------------------------------------------
+  // Main utility functions
+  //-------------------------------------------------
+
+  //get the text of a script tag that are storing shader code.
   static domShaderSrc(elmID) {
     var elm = document.getElementById(elmID);
     if (!elm || elm.text == '') {
@@ -9,6 +77,7 @@ class ShaderUtil {
     return elm.text;
   }
 
+  //Create a shader by passing in its code and what type
   static createShader(gl, src, type) {
     var shader = gl.createShader(type);
     gl.shaderSource(shader, src);
@@ -27,11 +96,18 @@ class ShaderUtil {
     return shader;
   }
 
+  //Link two compiled shaders to create a program for rendering.
   static createProgram(gl, vShader, fShader, doValidate) {
     //Link shaders together
     var prog = gl.createProgram();
     gl.attachShader(prog, vShader);
     gl.attachShader(prog, fShader);
+
+    //Force predefined locations for specific attributes. If the attibute isn't used in the shader its location will default to -1
+    gl.bindAttribLocation(prog, ATTR_POSITION_LOC, ATTR_POSITION_NAME);
+    gl.bindAttribLocation(prog, ATTR_NORMAL_LOC, ATTR_NORMAL_NAME);
+    gl.bindAttribLocation(prog, ATTR_UV_LOC, ATTR_UV_NAME);
+
     gl.linkProgram(prog);
 
     //Check if successful
@@ -71,16 +147,41 @@ class ShaderUtil {
   static domShaderProgram(gl, vectID, fragID, doValidate) {
     var vShaderTxt = ShaderUtil.domShaderSrc(vectID);
     if (!vShaderTxt) return null;
-
     var fShaderTxt = ShaderUtil.domShaderSrc(fragID);
     if (!fShaderTxt) return null;
-
     var vShader = ShaderUtil.createShader(gl, vShaderTxt, gl.VERTEX_SHADER);
     if (!vShader) return null;
-
     var fShader = ShaderUtil.createShader(gl, fShaderTxt, gl.FRAGMENT_SHADER);
-    if (!fShader) return null;
+    if (!fShader) {
+      gl.deleteShader(vShader);
+      return null;
+    }
 
     return ShaderUtil.createProgram(gl, vShader, fShader, true);
+  }
+
+  static createProgramFromText(gl, vShaderTxt, fShaderTxt, doValidate) {
+    var vShader = ShaderUtil.createShader(gl, vShaderTxt, gl.VERTEX_SHADER);
+    if (!vShader) return null;
+    var fShader = ShaderUtil.createShader(gl, fShaderTxt, gl.FRAGMENT_SHADER);
+    if (!fShader) {
+      gl.deleteShader(vShader);
+      return null;
+    }
+
+    return ShaderUtil.createProgram(gl, vShader, fShader, true);
+  }
+
+  //-------------------------------------------------
+  // Setters / Getters
+  //-------------------------------------------------
+
+  //Get the locations of standard Attributes that we will mostly be using. Location will = -1 if attribute is not found.
+  static getStandardAttribLocations(gl, program) {
+    return {
+      position: gl.getAttribLocation(program, ATTR_POSITION_NAME),
+      norm: gl.getAttribLocation(program, ATTR_NORMAL_NAME),
+      uv: gl.getAttribLocation(program, ATTR_UV_NAME)
+    };
   }
 }
